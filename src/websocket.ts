b5f776f1ece5fc322f7334527d021ef530a720b4
Christian Nuss <christian.nuss@gmail.com>
2021-11-21 14:35:00 -0800

    basic websocket

---
+++ src/websocket.ts
@@ -0,0 +1,47 @@
+import { APIGatewayProxyResultV2 } from 'aws-lambda';
+
+export const handler = async (
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  event: any,
+  context: unknown,
+): Promise<APIGatewayProxyResultV2> => {
+  console.log('Event:', event);
+  console.log('Context:', context);
+
+  const { requestContext } = event;
+  const { routeKey, connectionId } = requestContext;
+
+  console.log('!!! routekey', routeKey);
+
+  switch (routeKey) {
+    case '$connect': {
+      console.log('!!! checking token');
+      const token = (event.queryStringParameters && event.queryStringParameters.token) || undefined;
+      if (!token) {
+        console.log('!!! token not defined');
+        return { statusCode: 401, body: JSON.stringify({ error: 'Unauthorized' }) };
+      }
+      // TODO For Don: Check token
+
+      console.log(`Connected! Connection ID is ${connectionId}`);
+
+      // DEVNOTE For Don: API Gateway connections keepalive for 10 minutes
+
+      // TODO For Don: Save the connection ID + identity
+      return { statusCode: 200, body: 'Connected.' };
+    }
+    case '$disconnect':
+      console.log(`Disconnected! Connection ID was ${connectionId}`);
+      return { statusCode: 200, body: 'Disconnected.' };
+    case '$default': {
+      console.log(`Message Received! Connection ID is ${connectionId}`);
+
+      const { body } = event;
+      console.log('Message body', body); // DEVNOTE: Probably send stringify'd JSON and use JSON.parse(body) here
+
+      return { statusCode: 200, body: JSON.stringify({ message: `I got your message: ${body}` }) };
+    }
+    default:
+      return { statusCode: 200, body: JSON.stringify({ error: 'Unknown route key' }) };
+  }
+};
